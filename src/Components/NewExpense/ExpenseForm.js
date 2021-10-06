650a77e476a2baade53f6fef9706e1126ccafd28
incapsulate <incapsulate@incapsulates-MacBook-Pro.local>
2021-10-06 12:47:05 +0530

    added chart component

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const eventData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(eventData);
